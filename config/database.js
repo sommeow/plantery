ef46c96e190a0bafd45a2cd5ac70bd30f0c6b2ab
sommeow <s.pimson@gmail.com>
2019-05-23 15:34:28 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+// database connection event
+mongoose.connection.on('connected', function () {
+  console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
+});
+
+module.exports = mongoose;
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/', function(req, res, next) {
+  console.log(`plantName: ${req.body.plantName}`);
+  res.render('index');
+});
+
+module.exports = router;
